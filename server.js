cd4d41aec4049e67f88aecccbb696aad1436fd39
Kevin Liu <kevin07liu@gmail.com>
2017-06-25 18:36:48 +0800

    Change the port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+var port = process.env.PORT || 3000
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -31,6 +32,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Serever is up on port 3000');
+app.listen(port, () => {
+  console.log(`Serever is up on port ${port}`);
 });
