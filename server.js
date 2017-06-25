01c4eef86c7eb6cd41db81466f4491f0d1b692a5
Kevin Liu <kevin07liu@gmail.com>
2017-06-25 18:29:51 +0800

    This is a simple node app gonna gon online

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    message: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
+  })
+});
+
+app.get('/about', (req, res)=> {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage:[
+      'Something Wrong'
+    ]
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Serever is up on port 3000');
+});
